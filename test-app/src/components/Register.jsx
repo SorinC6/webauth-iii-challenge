a11d6bc060732672e7d9d439adfb3324d2ba2505
Sorin Chis <kish.sorin@yahoo.com>
2019-04-03 16:16:59 +0300

    added axios , styled-components and react-router-dom to the project

---
+++ test-app/src/components/Register.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import { Link, withRouter } from 'react-router-dom';
+axios.defaults.withCredentials = true;
+
+class Register extends Component {
+	state = {
+		username: '',
+		password: ''
+	};
+
+	onRegister = (e) => {
+		e.preventDefault();
+		const userData = {
+			username: this.state.username,
+			password: this.state.password
+		};
+		axios
+			.post('http://localhost:4000/api/register', userData)
+			.then((res) => {
+				console.log(res.data);
+				this.props.history.push('/login');
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	onUserChange = (e) => {
+		this.setState({
+			username: e.target.value
+		});
+	};
+
+	onPasswordChange = (e) => {
+		this.setState({
+			password: e.target.value
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<h1 style={{ textAlign: 'center' }}>REGISTER</h1>
+				<Form onSubmit={this.onRegister}>
+					<input
+						placeholder="username"
+						onChange={this.onUserChange}
+						value={this.state.username}
+						name="username"
+					/>
+					<input
+						placeholder="password"
+						onChange={this.onPasswordChange}
+						value={this.state.password}
+						name="password"
+					/>
+					<button type="submit">Register</button>
+				</Form>
+				<p style={{ textAlign: 'center' }}>
+					<Link to="/login">Already have a account?</Link>
+				</p>
+			</div>
+		);
+	}
+}
+
+export default withRouter(Register);
+
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	max-width: 400px;
+	margin: 0 auto;
+	align-items: center;
+	margin-top: 100px;
+
+	input {
+		padding: 20px 50px;
+		outline: none;
+		font-size: 16px;
+	}
+	button {
+		padding: 10px;
+		margin: 5px;
+		width: 40%;
+		border-radius: 5px;
+		outline: none;
+	}
+`;
